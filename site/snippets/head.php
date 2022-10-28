f6c32ee7cd39a84acdb796e918a4925862d0ef1d
JSMucho <john@wearemucho.com>
2022-10-28 23:28:33 +1100

    fix css

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $site->title() ?></title>
     <link rel="icon" type="image/png" href="assets/img/mucho-favicon.png" />
-    <?= css('assets/css/index.css', 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css') ?>
+    <?= css('assets/css/index.css') ?>
     <?= js('https://cdn.splitbee.io/sb.js', ['async' => true]) ?>
+    <?= css('https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css') ?>
     <?= js('https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js') ?>
 </head>
