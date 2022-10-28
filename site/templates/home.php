4bd555ca2b346e3bdaf0085f512f5e925aff1977
JSMucho <john@wearemucho.com>
2022-10-29 00:39:31 +1100

    update form

--- site/templates/home.php
+++ site/templates/home.php
@@ -6,27 +6,7 @@
         <main>
         </main>
 
-        <footer>
-            <div id="mc_embed_signup">
-                <form action="https://wearemucho.us14.list-manage.com/subscribe/post?u=ec11f7f20d2e3b7a621a61bc3&amp;id=6ad7d35609&amp;f_id=00dffee0f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate subscribe" target="_blank" novalidate>
-                    <div id="mc-field-group">
-                        <input type="email" value="Subscribe" onfocus="this.value=''" name="EMAIL" id="mce-EMAIL" class="required email" required>
-                        <div class="progress"></div>
-                        <span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span>
-                    </div>
-                    <div id="mce-responses" class="clear foot">
-                        <div class="response" id="mce-error-response" style="display:none"></div>
-                        <div class="response" id="mce-success-response" style="display:none"></div>
-                    </div>
-                    <div style="position: absolute; left: -5000px;" aria-hidden="true">
-                        <input type="text" name="b_ec11f7f20d2e3b7a621a61bc3_6ad7d35609" tabindex="-1" value="">
-                    </div>
-                    <div id="mc-submit" class="clear foot">
-                        <input type="image" src="assets/img/mucho-arrow.svg" alt="Submit" name="subscribe" id="mc-embedded-subscribe" class="button">
-                    </div>
-                </form>
-            </div>            
-        </footer>
+        <?php snippet('footer') ?>
     </div>
 </body>
 <div class="scripts">
