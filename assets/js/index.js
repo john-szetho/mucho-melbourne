34858124ae7adc7c7b22dae8ab9152e5bf4b5183
JSMucho <john@wearemucho.com>
2022-11-28 18:33:04 +1100

    fix subscribe form

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,50 +6,59 @@ var subscribeInput = document.querySelector('#mc-field-group input');
 var subscribeInputOG = document.querySelector('#mc-field-group input').value;
 var submitBtn = document.querySelector('#mc-submit input');
 
-form.onmouseover = function () {
-    if (subscribeInput.value == subscribeInputOG) {
-        subscribeInput.value = "Enter your email";
-    }
-}
-form.onmouseout = function () {
-    if (subscribeInput.value == "") {
-        subscribeInput.value = subscribeInputOG;
-
-        container.classList.remove('typing-container');
-        subscribeInput.classList.remove('typing');
-        submitBtn.classList.remove('typing');
-    } else {
-        subscribeInput.value = subscribeInputOG;
-    }
-}
-
-form.addEventListener('click', () => {
-    form.onmouseover = function () {
-        return false
-    }
-    form.onmouseout = function () {
-        return false
-    }, { once: true };
-})
-
 subscribeInput.onkeydown = function () {
     container.classList.add('typing-container');
     subscribeInput.classList.add('typing');
     submitBtn.classList.add('typing');
 }
 
-document.addEventListener('mouseover', () => {
-    if (subscribeInput.value == "") {
-        subscribeInput.value = "Subscribe";
-
-        form.onmouseover = function () {
-            subscribeInput.value = "Enter your email";
-        }
-        form.onmouseout = function () {
-            subscribeInput.value = subscribeInputOG;
-        }
+form.onmouseout = function () {
+    if (subscribeInput.value == subscribeInputOG || value) {
+        container.classList.remove('typing-container');
+        subscribeInput.classList.remove('typing');
+        submitBtn.classList.remove('typing');
     }
-})
+}
+
+// form.onmouseover = function () {
+//     if (subscribeInput.value == subscribeInputOG) {
+//         subscribeInput.value = "Enter your email";
+//     }
+// }
+
+// form.onmouseout = function () {
+//     if (subscribeInput.value == "") {
+//         subscribeInput.value = subscribeInputOG;
+
+//         container.classList.remove('typing-container');
+//         subscribeInput.classList.remove('typing');
+//         submitBtn.classList.remove('typing');
+//     } else {
+//         subscribeInput.value = subscribeInputOG;
+//     }
+// }
+
+// form.addEventListener('click', () => {
+//     form.onmouseover = function () {
+//         return false
+//     }
+//     form.onmouseout = function () {
+//         return false
+//     }, { once: true };
+// })
+
+// document.addEventListener('mouseover', () => {
+//     if (subscribeInput.value == "") {
+//         subscribeInput.value = "Subscribe";
+
+//         form.onmouseover = function () {
+//             subscribeInput.value = "Enter your email";
+//         }
+//         form.onmouseout = function () {
+//             subscribeInput.value = subscribeInputOG;
+//         }
+//     }
+// })
 
 
 // Mailchimp sign up script
