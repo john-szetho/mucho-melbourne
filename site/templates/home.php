31222c90e3c4178e1a2c074af2701b3c8e6132a0
JSMucho <john@wearemucho.com>
2022-12-03 11:19:00 +1100

    update styles

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,13 +1,10 @@
 <?php snippet('head') ?>
 <body>
-    <div class="container">
+    <main class="<?= $page->uid() ?>">
         <?php snippet('header') ?>
 
-        <main>
-        </main>
-
         <?php snippet('footer') ?>
-    </div>
+    </main>
 </body>
 <div class="scripts">
     <?= js(['https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js', 'assets/js/index.js', '//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js']) ?>
