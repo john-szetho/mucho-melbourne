228c61e2a67970d9180c642c977bd4b2e354d162
JSMucho <john@wearemucho.com>
2022-12-08 17:25:22 +1100

    fix button for safari

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -2,7 +2,5 @@
     <div class="logo">
         <a class="ext" href="https://wearemucho.com/" target="_blank"><img src="assets/img/mucho-logo.svg" alt="Mucho"></a>
     </div>
-    <div class="tagline">
-        <button onclick="window.location.reload();">Melbourne</button>
-    </div>
+    <button onclick="window.location.reload();"><h1>Melbourne</h1></button>
 </header>
